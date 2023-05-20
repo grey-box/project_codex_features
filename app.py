165b492a35698a2edb99e02e6930f5e9927d2385
abhishek-nairr <anair29@asu.edu>
2023-05-19 22:47:55 -0700

    with one change for port and host

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def get_phonetic_transcription(text):
         return jsonify({'phonetic_transcription': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
